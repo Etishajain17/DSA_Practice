4117302079145a1c29b2b4453344097bb154cf5c
EtishaJain17 <jainetisha0714>
2022-01-18 14:40:59 +0530

    Integer_repeated.java

--- src/Day_1/Integer_repeated.java
+++ src/Day_1/Integer_repeated.java
@@ -1,9 +1,6 @@
 package Day_1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Integer_repeated {
 
@@ -13,20 +10,25 @@ public class Integer_repeated {
         System.out.print("Enter the size of the array : ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             arr[i] = sc.nextInt();
         }
 
         HashMap<Integer, ArrayList> h = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (h.containsKey(arr[i])) {
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (h.containsKey(arr[i]))
+            {
                 h.get(arr[i]).add(i);
                 //h.replace(arr[i],h.replace(arr[i],h.get(arr[i])));    // it is not required b'z reference passed
-            } else {
+            }
+            else
+            {
 
                 h.put(arr[i], new ArrayList(Arrays.asList(i)));
             }
         }
-        System.out.println(h);
+
     }
 }
