ac388fa0dd13a87463464e8a4cc8ed8a12a9cce0
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-23 13:14:50 +0530

    XPowerN

---
+++ src/Day_5/XPowerN1.java
@@ -0,0 +1,26 @@
+package Day_5;
+
+import java.util.Scanner;
+
+public class XPowerN1
+{
+    public int power1(int x,int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        return x * power1(x,n-1);
+    }
+}
+class main1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        XPowerN1 obj=new XPowerN1();
+        System.out.println(obj.power1(x,n));
+    }
+}
---
+++ src/Day_5/XPowerN2.java
@@ -0,0 +1,28 @@
+package Day_5;
+
+import java.util.Scanner;
+
+public class XPowerN2
+{
+    public void power2(int x,int n,int p)
+    {
+        if(n==0)
+        {
+            System.out.println(p);
+            return;
+        }
+        power2(x,n-1,p*x);
+    }
+}
+class main2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        XPowerN2 obj=new XPowerN2();
+        int p=1;
+        obj.power2(x,n,p);
+    }
+}
