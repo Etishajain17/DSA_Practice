4e4fc3664e1cff3332338f56080b72486b352c67
EtishaJain17 <jainetisha0714@gmail.com>
2022-01-27 14:04:52 +0530

    Printing all numbers of given range in lexiographical order i.e. dictionary like.

---
+++ src/Day_7/Lexiographically.java
@@ -0,0 +1,30 @@
+package Day_7;
+
+import java.util.Scanner;
+
+public class Lexiographically
+{
+    public static void order(int initial,int end)
+    {
+        if(initial>end)
+            return;
+        System.out.print(initial+" ");
+        int i=0;
+        if(initial==0)
+        {
+            i=1;
+        }
+        while(i<10)
+        {
+            order(initial*10+i,end);
+            i++;
+        }
+    }
+}
+class main1
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Lexiographically.order(sc.nextInt(),sc.nextInt());
+    }
+}
